98b526c809a6907cd89900a85b05a8d54d3ffaab
ligang <ligang@andlinks.com>
2018-09-01 14:53:36 +0800

    m

--- example.cu
+++ example.cu
@@ -10,7 +10,7 @@ __global__ void hello(){
 int main(void){
   int num_threads = STR_LENGTH;
   int num_blocks = 1;
-  hello<<<num_blocks, num_threads>>>()
+  hello<<<num_blocks, num_threads>>>();
   //  等待所有的host 线程中执行的命令执行完毕
   cudaDeviceSynchronize();
   return 0;
